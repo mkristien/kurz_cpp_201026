fd8467874bca6e32cef2ad643fb6a864b09680dc
Martin Kristien <martin.kristien@gmail.com>
2020-10-27 09:57:57 +0100

    Male upravy v hello.cpp

--- src/lekcia_1/hello.cpp
+++ src/lekcia_1/hello.cpp
@@ -1,7 +1,9 @@
 
+// pridame do programu standardnu kniznicu na input/output
 #include <stdio.h>
 
 int main () {
+    // jednoduchy output, text sa zobrazi v terminaly
     printf("Hello AjTyvIt");
     return 0;
 }
