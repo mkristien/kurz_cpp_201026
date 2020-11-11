db5ae0b2f99452a7d350a187d27efa2fcae071bf
Martin Kristien <martin.kristien@gmail.com>
2020-11-11 06:41:24 +0000

    Zmen zakladne funkcie aby napouzivali staticke data

--- hadik/src/main.cpp
+++ hadik/src/main.cpp
@@ -4,60 +4,76 @@
 #include "config.h"
 
 // nove datove typy
+enum smer { dolava, doprava, dole, hore, ziaden};
+
 struct bod {
   int x;  // stlpec
   int y;  // riadok
 };
-enum smer { dolava, doprava, dole, hore, ziaden};
 
-// datove premenne v statickom priestore
-char  mriezka[RIADKY][STLPCE];
+struct meta_had {
+  bod*  had;
+  int   had_dlzka;
+  smer  had_smer;
+  bod   vymazany_chvost;
+};
 
-bod   had[STLPCE*RIADKY];
-int   had_dlzka;
-smer  had_smer;
+struct meta_mriezka {
+  int     riadky;
+  int     stlpce;
+  char**  mriezka;
+};
+
+meta_had     data_had;
+meta_mriezka data_mriezka;
 
 /**
  * Vytvor hada na zaciatku hry
  */
-void init_had() {
-  had[0].x = 1;
-  had[0].y = 1;
+meta_had* init_had(int max_dlzka) {
+  data_had.had = (bod*) malloc(max_dlzka * sizeof(bod));
+  data_had.had[0] = {1, 1};
+  data_had.had[1] = {2, 1};
+  data_had.had[2] = {3, 1};
+  data_had.had[3] = {4, 1};
+  data_had.had[4] = {5, 1};
 
-  had[1] = {2, 1};
-  had[2] = {3, 1};
-  had[3] = {4, 1};
-  had[4] = {5, 1};
+  data_had.vymazany_chvost = {6, 1};
 
-  had_dlzka = 5;
-  had_smer  = doprava;
+  data_had.had_dlzka = 5;
+  data_had.had_smer  = doprava;
+  return &data_had;
 }
 
 /**
  * Zobraz data z hada v mriezke
  */
-void had_do_mriezky() {
+void had_do_mriezky(meta_had* h, char** mriezka) {
+  // zmen vymazany chvost na "prazdne" v mriezke
+  mriezka[h->vymazany_chvost.y][h->vymazany_chvost.x] = ' ';
+
   // telo
-  for (int i=0; i<had_dlzka-1; i++) {
-    bod pozicia = had[i];
+  for (int i=0; i< h->had_dlzka -1; i++) {
+    bod pozicia = h->had[i];
     mriezka[pozicia.y][pozicia.x] = 'X';
   }
 
   // hlava
-  bod hlava = had[had_dlzka-1];
+  bod hlava = h->had[h->had_dlzka-1];
   mriezka[hlava.y][hlava.x] = 'O';
 }
 
 /**
  * Zmen data o hadovi podla toho, ktorym smerom sa hybe
  */
-void had_pohyb(smer novy_smer) {
+void had_pohyb(meta_had* h, smer novy_smer) {
   // poloha novej hlavy sa posunie o 1 urcitym smerom od povodnej hlavy
-  bod nova_hlava = had[had_dlzka-1];
+  int dlzka       = h->had_dlzka;
+  bod nova_hlava  = h->had[dlzka-1];
 
   // ak sa smer nemeni, pouzi povodny smer
   if (novy_smer == ziaden) {
-    novy_smer = had_smer;
+    novy_smer = h->had_smer;
   }
 
   switch (novy_smer) {
@@ -77,11 +93,12 @@ void had_pohyb(smer novy_smer) {
       break;
   }
 
-  had_smer        = novy_smer;
-  had[had_dlzka]  = nova_hlava;
+  h->had_smer   = novy_smer;
+  h->had[dlzka] = nova_hlava;
   // vymaz chvost z hada
-  for (int i=0; i<had_dlzka; i++) {
-    had[i] = had[i+1];
+  h->vymazany_chvost = h->had[0];
+  for (int i=0; i< dlzka; i++) {
+    h->had[i] = h->had[i+1];
   }
 }
 
@@ -92,7 +109,21 @@ void had_pohyb(smer novy_smer) {
  * |                |
  * ------------------
  */
-void init_mriezka() {
+meta_mriezka* init_mriezka(int riadky, int stlpce) {
+  /**
+   * mriezka
+   * [0]        -> [0, 1, 3, 4, ..., stlpce-1]
+   * [1]        -> [0, 1, 3, 4, ..., stlpce-1]
+   * [2]        -> [0, 1, 3, 4, ..., stlpce-1]
+   * [3]        -> [0, 1, 3, 4, ..., stlpce-1]
+   * ...
+   * [riadky-1] -> [0, 1, 3, 4, ..., stlpce-1]
+   */
+  char** mriezka = (char**) malloc(riadky * sizeof(char*));
+  for (int i=0; i<riadky; i++) {
+    mriezka[i] = (char*) malloc(stlpce*sizeof(char));
+  }
+
   // prvy riadok
   for (int i=0; i < STLPCE; i++) {
     mriezka[0][i] = '-';
@@ -114,14 +145,19 @@ void init_mriezka() {
   for (int i=0; i < STLPCE; i++) {
     mriezka[RIADKY-1][i] = '-';
   }
+
+  data_mriezka.mriezka = mriezka;
+  data_mriezka.riadky  = riadky;
+  data_mriezka.stlpce  = stlpce;
+  return &data_mriezka;
 }
 
-void print_mriezka() {
+void print_mriezka(meta_mriezka* m) {
   printf("\e[2J\e[H");    // vycisti terminal a daj kurzor na poziciu 0,0
 
-  for (int i=0; i<RIADKY; i++) {
-    for (int j=0; j<STLPCE; j++) {
-      putchar(mriezka[i][j]);   // printf("%c", char)
+  for (int i=0; i < m->riadky; i++) {
+    for (int j=0; j < m->stlpce; j++) {
+      putchar(m->mriezka[i][j]);   // printf("%c", char)
     }
     putchar('\n');
   }
@@ -150,10 +186,10 @@ smer klavesa_na_smer(char klavesa) {
 }
 
 int main() {
-  init_mriezka();
-  init_had();
-  had_do_mriezky();
-  print_mriezka();
+  meta_mriezka* m = init_mriezka(RIADKY, STLPCE);
+  meta_had*     h = init_had(RIADKY*STLPCE);
+  had_do_mriezky(h, m->mriezka);
+  print_mriezka(m);
 
   // nastav terminal tak, aby sa klavesy:
   // - dostali do vstupu hned (nie az po stlaceni ENTER)
@@ -179,12 +215,11 @@ int main() {
     smer novy_smer = klavesa_na_smer(klavesa);
 
     // 3
-    had_pohyb(novy_smer);
+    had_pohyb(h, novy_smer);
 
     // 4
-    init_mriezka();
-    had_do_mriezky();
-    print_mriezka();
+    had_do_mriezky(h, m->mriezka);
+    print_mriezka(m);
   }
 
   // obnov povodne nastevenie terminalu
